fa763f1a031bcff22533a8f1017cc7b85984cdc8
nicoarias47 <ariasn1995@gmail.com>
2022-07-27 21:15:46 -0300

    pages part1

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetchData();
+  pintarPage();
 });
 
-fetchData = async () => {
+fetchData = async (page) => {
   try {
     loading(true);
-
     const resp = await fetch(
-      "https://rickandmortyapi.com/api/character?page=1"
+      "https://rickandmortyapi.com/api/character?page=" + page
     );
     const data = await resp.json();
 
@@ -18,7 +17,7 @@ fetchData = async () => {
     loading(false);
   }
 };
-
+// LOADING
 const loading = (estado) => {
   const load = document.querySelector("#loading");
 
@@ -29,6 +28,7 @@ const loading = (estado) => {
   }
 };
 
+// PINTAR CARDS
 const pintarCards = (data) => {
   const container = document.querySelector(".card-container");
   const templateCards = document.querySelector("#template-cards").content;
@@ -43,4 +43,86 @@ const pintarCards = (data) => {
   });
 
   container.appendChild(fragment);
+};
+
+// -- remove cards --
+const removeCards = () => {
+  const cards = document.querySelectorAll(".article");
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].remove();
+  }
+};
+
+// PAGINACION
+let page = 1;
+
+const pageOne = document.querySelector("#pageOne");
+const pageTwo = document.querySelector("#pageTwo");
+const pageThree = document.querySelector("#pageThree");
+
+const pintarPage = () => {
+  const pages = 42;
+
+  const firstPage = document.querySelector("#firstPage");
+  firstPage.addEventListener("click", () => {
+    if (page > 1) {
+      page = 1;
+      removeCards();
+      fetchData(page);
+      navPage();
+    }
+  });
+
+  const lastPage = document.querySelector("#lastPage");
+  lastPage.addEventListener("click", () => {
+    if (page < 42) {
+      page = pages;
+      removeCards();
+      fetchData(page);
+      navPage();
+    }
+  });
+
+  if (page < 3) {
+    pageOne.addEventListener("click", () => {
+      page = 1;
+      removeCards();
+      return fetchData(page);
+    });
+
+    pageTwo.addEventListener("click", () => {
+      page = 2;
+      removeCards();
+      return fetchData(page);
+    });
+
+    pageThree.addEventListener("click", () => {
+      page = 3;
+      removeCards();
+      return fetchData(page);
+    });
+  }
+
+  if (page >= 2) {
+    pageOne.addEventListener("click", () => {
+      page--;
+      removeCards();
+      fetchData(page);
+      navPage();
+    });
+    pageThree.addEventListener("click", () => {
+      page++;
+      removeCards();
+      fetchData(page);
+      navPage();
+    });
+  }
+
+  fetchData(page);
+};
+
+const navPage = () => {
+  pageOne.textContent = page - 1;
+  pageTwo.textContent = page;
+  pageThree.textContent = page + 1;
 };
